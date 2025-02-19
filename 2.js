5f4df540e1cf17104740025121a9f44a8accd674
mohamedelshafie33 <mohamedelshafie33@gmail.com>
2025-02-19 20:34:51 +0200

    1

--- 2.js
+++ 2.js
@@ -3,44 +3,34 @@ let prices = ["1000 le", "1100 le", "1200 le"];
 let quantities = ["10 phones", "20 phones", "30 phones"];
 
 let showdata=()=>{
+    console.clear();
+
     phones.forEach((phone, index)=>{
-        console.log(`Phone: ${phone}/ Price: ${prices[index]}/ Quantity: ${quantities[index]}`);
+        console.log(`${index+1} -Phone: ${phone}/ Price: ${prices[index]}/ Quantity: ${quantities[index]}`);
     })
 }
 showdata();
 
 
 let adding=()=>{
-    let phone = prompt("Enter phone name");
-    let price = prompt("Enter phone name");
-    let quantitie = prompt("Enter phone name");
 
-    phones.push(phone);
-    prices.push(price);
-    quantities.push(quantitie);
-    
+    phones.push( prompt("Enter phone name"));
+    prices.push(prompt("Enter phone name"));
+    quantities.push( prompt("Enter phone name"));
     showdata();
 }
 let deleteing=()=>{
     let phone = +prompt("Enter phone number to delete")-1;
-    let price =phone;
-    let quantitie =phone;
-
     phones.splice(phone,1);
-    prices.splice(price,1);
-    quantities.splice(quantitie,1);
+    prices.splice(phone,1);
+    quantities.splice(phone,1);
     
     showdata();
 }
 let uptade=()=>{
-    let phone = +prompt("Enter phone number to delete")-1;
-    let price =phone;
-    let quantitie =phone;
-    
-
     phones.splice(phone,1,prompt("Enter phone name"));
-    prices.splice(price,1,prompt("Enter phone price"));
-    quantities.splice(quantitie,1,prompt("Enter phone quantity"));
+    prices.splice(phone,1,prompt("Enter phone price"));
+    quantities.splice(phone,1,prompt("Enter phone quantity"));
     
     showdata();
 }
