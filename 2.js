48ac0899bc959f1e916573e1a28d9a36e1d12eb7
mohamedelshafie33 <mohamedelshafie33@gmail.com>
2025-02-20 00:35:55 +0200

    2

--- 2.js
+++ 2.js
@@ -1,12 +1,12 @@
 let phones = ["iphone x","iphone 11","iphone 12"];
-let prices = ["1000 le", "1100 le", "1200 le"];
-let quantities = ["10 phones", "20 phones", "30 phones"];
+let prices = ["1000", "1100", "1200"];
+let quantities = ["10", "20", "30"];
 
 let showdata=()=>{
     console.clear();
 
     phones.forEach((phone, index)=>{
-        console.log(`${index+1} -Phone: ${phone}/ Price: ${prices[index]}/ Quantity: ${quantities[index]}`);
+        console.log(`${index+1} - ${phone}/ Price: ${prices[index]} le/ Quantity: ${quantities[index]} phones`);
     })
 }
 showdata();
@@ -27,10 +27,28 @@ let deleteing=()=>{
     
     showdata();
 }
-let uptade=()=>{
+let update=()=>{
+    let phone = +prompt("Enter phone number to uptade")-1;
     phones.splice(phone,1,prompt("Enter phone name"));
     prices.splice(phone,1,prompt("Enter phone price"));
     quantities.splice(phone,1,prompt("Enter phone quantity"));
     
     showdata();
 }
+
+
+let update1=()=>{
+    let phone = +prompt("Enter phone number to uptade")-1;
+    let updatetype = prompt("Enter uptade type");
+    if(updatetype == "phone"){
+        phones.splice(phone,1,prompt("Enter phone name"));    
+    }
+    else if(updatetype == "price"){
+        prices.splice(phone,1,prompt("Enter phone price"));    
+    }
+    else if(updatetype == "quantity"){
+        quantities.splice(phone,1,prompt("Enter phone quantity"));    
+    }
+    showdata();
+}
+
